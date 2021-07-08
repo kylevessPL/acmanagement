9381877e35ede2e2fafc5ad55ab6889ee9dfaf55
kylevessPL <kacperpiasta@gmail.com>
2021-07-08 12:04:41 +0200

    fix datetime not correctly serialized

--- application/web-application-parent/web-application-api/src/main/java/pl/piasta/acmanagement/api/misc/JsonPatchConfig.java
+++ application/web-application-parent/web-application-api/src/main/java/pl/piasta/acmanagement/api/misc/JsonPatchConfig.java
@@ -3,6 +3,7 @@ package pl.piasta.acmanagement.api.misc;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,7 @@ public class JsonPatchConfig {
         return new ObjectMapper()
                 .setDefaultPropertyInclusion(Include.NON_NULL)
                 .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .findAndRegisterModules();
     }
 }
